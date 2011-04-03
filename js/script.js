0ea1a498c4891c9f73411f8070909b5c7bd0fb47
Andrew Scala <agscala@gmail.com>
2011-04-03 15:09:52 -0400

    Works completely!

--- js/script.js
+++ js/script.js
@@ -3,40 +3,45 @@ Copyright (c) 2010, Patrick Wied. All rights reserved.
 Code licensed under the BSD License:
 http://patrick-wied.at/static/license.txt
 */
-var heatmapApp = (function(){
+var Heatmap = function(canvas_id, width, height) {
 	var self = this;
+
 	// var definition
 	// canvas: the canvas element
-	// ctx: the canvas 2d context
+	// context: the canvas 2d context
 	// width: the heatmap width for border calculations
 	// height: the heatmap height for border calculations
 	// invoke: the app doesn't react on the mouse events unless the invoke var is set to true
-	var canvas,
-		ctx,
-		width,
-		height,
-		radius1 = 20,
-		radius2 = 40,
-		invoke = false;
+	this.canvas = document.getElementById(canvas_id);
+	this.canvas.width = width;
+	this.canvas.height = height;
+	this.context = this.canvas.getContext("2d");
+	this.width = width;
+	this.height = height;
+
+	this.radius1 = 20;
+	this.radius2 = 40;
+
 	// function for coloring the heatmap
-	var colorize = function(x,y,x2){
+	this.colorize = function(x, y, x2) {
 		// initial check if x and y is outside the app
 		// -> resetting values
-		if(x+x2>width)
-			x=width-x2;
-		if(x<0)
-			x=0;
-		if(y<0)
-			y=0;
-		if(y+x2>height)
-			y=height-x2;
+		if(x+x2 > width)
+			x = width-x2;
+		if(x < 0)
+			x = 0;
+		if(y < 0)
+			y = 0;
+		if(y+x2 > height)
+			y = height-x2;
 		// get the image data for the mouse movement area
-		var image = ctx.getImageData(x,y,x2,x2),
+		var image = this.context.getImageData(x, y, x2, x2);
 		// some performance tweaks
-			imageData = image.data,
-			length = imageData.length;
+		var imageData = image.data;
+		var length = imageData.length;
+
 		// loop thru the area
-		for(var i=3; i < length; i+=4){
+		for(var i = 3; i < length; i += 4) {
 
 			var r = 0,
 				g = 0,
@@ -50,30 +55,29 @@ var heatmapApp = (function(){
 				tmp=255-alpha;
 				r=255-tmp;
 				g=tmp*12;
-			}else if(alpha<=234 && alpha >= 200){
+			} else if(alpha<=234 && alpha >= 200){
 				tmp=234-alpha;
 				r=255-(tmp*8);
 				g=255;
-			}else if(alpha<= 199 && alpha >= 150){
+			} else if(alpha<= 199 && alpha >= 150){
 				tmp=199-alpha;
 				g=255;
 				b=tmp*5;
-			}else if(alpha<= 149 && alpha >= 100){
+			} else if(alpha<= 149 && alpha >= 100){
 				tmp=149-alpha;
 				g=255-(tmp*5);
 				b=255;
-			}else
+			} else
 				b=255;
-			// we ve started with i=3
-			// set the new r, g and b values
-			imageData[i-3]=r;
-			imageData[i-2]=g;
-			imageData[i-1]=b;
+
+			imageData[i-3] = r;
+			imageData[i-2] = g;
+			imageData[i-1] = b;
 		}
 		// the rgb data manipulation didn't affect the ImageData object(defined on the top)
 		// after the manipulation process we have to set the manipulated data to the ImageData object
 		image.data = imageData;
-		ctx.putImageData(image,x,y);
+		this.context.putImageData(image,x,y);
 	}
 
 	this.add_point = function(x, y) {
@@ -83,45 +87,27 @@ var heatmapApp = (function(){
 		}
 
 		// storing the variables because they will be often used
-		var r1 = radius1;
-		var r2 = radius2;
+		var r1 = this.radius1;
+		var r2 = this.radius2;
 
 		//console.log("x: "+x+"; y:" +y);
 		// create a radial gradient with the defined parameters. we want to draw an alphamap
-		var rgr = ctx.createRadialGradient(x,y,r1,x,y,r2);
+		var rgr = self.context.createRadialGradient(x, y, r1, x, y, r2);
 		// the center of the radial gradient has .1 alpha value
 		rgr.addColorStop(0, 'rgba(0,0,0,0.1)');
 		// and it fades out to 0
 		rgr.addColorStop(1, 'rgba(0,0,0,0)');
 		// drawing the gradient
-		ctx.fillStyle = rgr;
-		ctx.fillRect(x-r2,y-r2,2*r2,2*r2);
-		// negate the invoke variable
-		// next execution of the logic is when the activate method activates the invoke var again
-		invoke=!invoke;
-		// at least colorize the area
-		colorize(x-r2,y-r2,2*r2);
+		this.context.fillStyle = rgr;
+		this.context.fillRect(x-r2, y-r2, 2*r2, 2*r2);
+		// at last colorize the area
+		this.colorize(x-r2, y-r2, 2*r2);
 	};
 
-	return {
-		initialize: function(c, wt, ht){
-			canvas = document.getElementById(c);
-			canvas.width = wt;
-			canvas.height = ht;
-			ctx = canvas.getContext("2d");
-			width = wt;
-			height = ht;
-		},
-
-		add_point: function(x, y) {
-			self.add_point(x, y);
-		},
-
-		getData: function(){
-			return canvas.toDataURL();
-		}
-	};
-})();
+	this.getData = function(){
+		return this.canvas.toDataURL();
+	}
+};
 
 function set_instructions(text) {
     $("#instructions").html(text);
@@ -133,34 +119,31 @@ function next_num() {
 	return counter;
 }
 
-var IMAGE = 0,
-    HEATMAP = 1;
 
 var Image = function(url) {
     var self = this;
 	this.url = url;
-	this.div_string = "image-" + next_num();
+
+	var div_string = "image-" + next_num();
+	var heatmap_div_string = "heatmap-" + next_num();
 
 	$("#pictures").append(""+
-		"<div id='" + this.div_string +"' class='image-wrapper'>" +
+		"<div id='" + div_string +"' class='image-wrapper'>" +
 			"<div class='image absolute'><img src='" + url + "'/></div>" +
-			"<canvas id='heatmap' class='image-heatmap absolute'>" +
+			"<canvas id='" + heatmap_div_string + "' class='image-heatmap absolute'></canvas>" +
 			"<div class='clear'></div>" +
 		"</div>"
 	);
 
-    this.div = $("#" + this.div_string);
-    this.position = this.div.offset();
+    this.div = $("#" + div_string);
     this.state = IMAGE;
-	this.heatmap_div = this.div.find(".image-heatmap");
-	this.heatmap = heatmapApp.initialize("heatmap", 400, 300);
-	this.next_image = undefined;
+	this.heatmap = new Heatmap(heatmap_div_string, 400, 300);
 
     this.div.find(".image").click(function(event) {
         var x = event.offsetX;
         var y = event.offsetY;
-		heatmapApp.add_point(x, y);
 
+		self.heatmap.add_point(x, y);
 		self.generate_heatmap();
 
         $.ajax({
@@ -171,7 +154,6 @@ var Image = function(url) {
                 self.show_heatmap();
 				self.state = HEATMAP;
 
-				self.generate_next_image();
             },
         });
 
@@ -184,7 +166,7 @@ var Image = function(url) {
 			for(var i in points) {
 				var x = points[i].x
 				var y = points[i].y
-				heatmapApp.add_point(x, y);
+				self.heatmap.add_point(x, y);
 			}
         });
     };
@@ -193,34 +175,59 @@ var Image = function(url) {
         this.div.find(".image-heatmap").fadeIn();
     };
 
-    this.generate_next_image = function() {
-		$.getJSON("http://127.0.0.1:8124/random", function(data) {
-			var url = data['url'];
 
-			self.next_image = Image(url);
-			self.next_image.hide();
-		});
-    };
-
-    $("body").click(function(event) {
-        if(self.state == HEATMAP) {
-			self.hide();
-			self.next_image.show();
-        }
-    });
-
-	this.hide = function() {
+	this.disappear = function() {
 		self.div.fadeOut();
 	};
 
-	this.show = function() {
+	this.reveal = function() {
 		self.div.fadeIn();
         set_instructions("CLICK THE PICTURE");
 	};
 }
 
+var IMAGE = 0,
+    HEATMAP = 1;
+
+var ImageManager = (function() {
+	var self = this;
+	var current_image, next_image;
+
+	var get_next_image = function() {
+		$.getJSON("http://127.0.0.1:8124/random", function(data) {
+			var url = data['url'];
+
+			next_image = new Image(url);
+			next_image.disappear();
+		});
+	};
+
+	var initialize = function() {
+		$.getJSON("http://127.0.0.1:8124/random", function(data) {
+			var url = data['url'];
+
+			current_image = new Image(url);
+			current_image.reveal();
+			get_next_image();
+		});
+	}
+
+    $("body").click(function(event) {
+        if(current_image.state == HEATMAP) {
+			current_image.disappear();
+			current_image = next_image;
+			current_image.reveal();
+			get_next_image();
+        }
+    });
+
+	return {
+		initialize: initialize,
+	};
+})();
+
 $(document).ready(function() {
-    var image = new Image("http://placekitten.com/400/300");
+	ImageManager.initialize();
 
 	$("#new-image-submit").click(function(event) {
 		event.preventDefault();
